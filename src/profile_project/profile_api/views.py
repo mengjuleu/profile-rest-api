de9cd8294fe39417f8ddc0d6a85c098520b29ace
mengjuleu <mleu@atlassian.com>
2018-08-31 00:50:07 -0700

    add search function

--- src/profile_project/profile_api/views.py
+++ src/profile_project/profile_api/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
-from  rest_framework.views import APIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from . import serializers
 from . import models
@@ -112,4 +113,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.object.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
 
